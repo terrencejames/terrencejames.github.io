dc13970dc8453b59bd5d13d581463879800e8c21
Unknown <tdiaz@hmc.edu>
2019-12-17 14:55:48 -0800

    added animations to cv

--- assets/js/resume.js
+++ assets/js/resume.js
@@ -1,28 +1,5 @@
-(function($) {
-  "use strict"; // Start of use strict
-
-  // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: (target.offset().top)
-        }, 1000, "easeInOutExpo");
-        return false;
-      }
-    }
-  });
-
-  // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
-    $('.navbar-collapse').collapse('hide');
-  });
-
-  // Activate scrollspy to add active class to navbar items on scroll
-  $('body').scrollspy({
-    target: '#sideNav'
-  });
-
-})(jQuery); // End of use strict
+$(document).ready(function(){
+      ScrollReveal().reveal(".resume-section", { delay: 400, distance: '50px' });
+      ScrollReveal().reveal("#navigation", { delay: 400, distance: '50px', origin: 'left' });
+      ScrollReveal().reveal("#header", { delay: 400, distance: '50px', origin: 'top' });
+})
