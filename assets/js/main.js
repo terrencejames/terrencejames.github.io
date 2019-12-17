9d4e03a4ec3db8fbb129e736fef19602d407c3c7
Unknown <tdiaz@hmc.edu>
2019-12-17 15:47:51 -0800

    should not animate navigation at all on mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -44,8 +44,7 @@
             ScrollReveal().reveal("#softwareeng", { delay: 500, distance: '150px', afterReveal: null, desktop: false });
             ScrollReveal().reveal("#resumecv", { delay: 500, duration: 800,  distance: '150px', origin: 'right', desktop: false });
             ScrollReveal().reveal($header, {delay: 500, distance: '50px', afterReveal: null, desktop: false});
-            ScrollReveal().reveal("#navigation", { delay: 500, distance: '50px', origin: 'left', desktop: false });
-
+            
             ScrollReveal().reveal("#work-experiences", { delay: 500, distance: '50px' });
             ScrollReveal().reveal("#profile", { delay: 500, distance: '50px' });
 
