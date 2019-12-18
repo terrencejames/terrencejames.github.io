afd17e8a202398a62785d9740ec81f5a869d8f48
Unknown <tdiaz@hmc.edu>
2019-12-18 13:25:34 -0800

    move cv page back to top on refresh

--- assets/js/resume.js
+++ assets/js/resume.js
@@ -1,3 +1,7 @@
+$(window).on('beforeunload', function() {
+  $(window).scrollTop(0);
+});
+
 $(document).ready(function(){
       ScrollReveal().reveal(".resume-section", { delay: 400, distance: '50px' });
       ScrollReveal().reveal("#navigation", { delay: 400, distance: '50px', origin: 'left', mobile: false });
